6fad6aafa69f779c038cb9c5b4887386c427ff10
Limber Jhonathan <limberjhonathan@gmail.com>
2024-08-19 22:18:45 -0300

    protected

---
+++ src/A0025-protected/A0025-protected.ts
@@ -0,0 +1,52 @@
+export class Empresa {
+    public readonly nome: string;
+    protected readonly colaboradores: Colaborador[] = [];
+    private readonly cnpj: string;
+
+    constructor(nome: string, cnpj: string){
+        this.nome = nome;
+        this.cnpj = cnpj
+    }
+
+    adicionaColaborador(colaborador: Colaborador): void {
+        this.colaboradores.push(colaborador);
+    }
+
+    mostrarColaboradores(): void {
+        for (const colaborador of this.colaboradores){
+            console.log(colaborador)
+        }
+    }
+}
+
+export class Udemy extends Empresa {
+    constructor(){
+        super("Udemy", '11.111.111/0001-11')
+    }
+
+    popColaborador(): Colaborador | null {
+        const colaborador = this.colaboradores.pop();
+        if(colaborador) return colaborador;
+        return null
+    }
+}
+
+export class Colaborador {
+    constructor(
+        public readonly nome: string,
+        public readonly sobrenome: string,
+    ) {}
+}
+
+const empresa1 = new Udemy();
+const colaborador1 = new Colaborador('Luiz', 'Otávio');
+const colaborador2 = new Colaborador('Maria', 'Miranda');
+const colaborador3 = new Colaborador('João', 'Victor');
+const colaborador4 = new Colaborador('Carlos', 'Ferreira');
+empresa1.adicionaColaborador(colaborador1)
+empresa1.adicionaColaborador(colaborador2)
+empresa1.adicionaColaborador(colaborador3)
+empresa1.adicionaColaborador(colaborador4)
+const colaboradorRemovido = empresa1.popColaborador()
+console.log("Removido --> ",colaboradorRemovido);
+console.log(empresa1)
