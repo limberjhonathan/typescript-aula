bb57508f42cd0cab636d2addc0056262f6141c49
Limber Jhonathan <limberjhonathan@gmail.com>
2024-08-29 22:56:10 -0300

    generic 3

---
+++ src/A0045-generics-3/A0045-generics-3.ts
@@ -0,0 +1,30 @@
+interface PessoaProtocolo<T = string, U = number> {
+    nome: T;
+    sobrenome: T;
+    idade: U;
+}
+
+type PessoaProtocolo2<T = string, U = number> = {
+    nome: T;
+    sobrenome: T;
+    idade: U;
+}
+
+const aluno1: PessoaProtocolo<string, number> = {
+    nome: 'Luiz',
+    sobrenome: 'Miranda',
+    idade: 30,
+}
+//const aluno2: PessoaProtocolo<string, number> = {
+//    nome: 123,
+//    sobrenome: 456,
+//    idade: 30,
+//}
+const aluno3: PessoaProtocolo2= {
+    nome: 'Luiz',
+    sobrenome: 'Miranda',
+    idade: 30,
+}
+
+
+console.log(aluno1, aluno3)
