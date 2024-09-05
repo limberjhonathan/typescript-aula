7fc6bbed96ca14f4ed3cab72fd03bd1a7ab3083d
Limber Jhonathan <limberjhonathan@gmail.com>
2024-09-05 20:26:42 -0300

    decorador class 7

---
+++ src/A0057-decorator-class-7/A0057-decorator-class-7.ts
@@ -0,0 +1,50 @@
+function decorador(classPrototype: any, nome: string | symbol): any {
+    let valorPropriedade: any;
+
+    return {
+        get: () => valorPropriedade,
+        set: (valor: any) => {
+            if (typeof valor === 'string') {
+                valorPropriedade = valor.split('').reverse().join('')
+                return
+            }
+            valorPropriedade = valor
+        } 
+    }
+}
+
+export class UmaPessoa {
+    @decorador
+    nome: string;
+    @decorador
+    sobrenome: string;
+    @decorador
+    idade: number;
+
+    constructor(nome: string, sobrenome: string, idade: number) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.idade = idade;
+    }
+       
+    metodo(msg: string): string {
+        return `${this.nome} ${this.sobrenome}: ${msg}`
+    }
+
+    get nomeCompleto(): string {
+        return this.nome + ' ' + this.sobrenome;
+    }
+
+    set nomeCompleto(valor: string) {
+        const palavras = valor.split(/\s+/g);
+        const primeiroNome = palavras.shift();
+        if(!primeiroNome) return;
+        this.nome = primeiroNome;
+        this.sobrenome = palavras.join(' ');
+    }
+}
+
+const pessoa = new UmaPessoa('Luiz', 'Otavio', 30)
+//pessoa.metodo = () => 'EIIIIIIIIIIIIIIIIIIIIII'
+const metodo = pessoa.metodo('Ola mundo')
+console.log(metodo)
