6598c350f44d1f35eea9fa7a087fa5d3c895021f
Limber Jhonathan <limberjhonathan@gmail.com>
2024-09-05 20:25:43 -0300

    decorador class 5

---
+++ src/A0055-decorator-class-5/A0055-decorator-class-5.ts
@@ -0,0 +1,44 @@
+function decorador(classPrototype: any, nomeMetodo: string, descriptor: PropertyDescriptor): PropertyDescriptor | void {
+    console.log('---->', classPrototype);
+    console.log('---->', nomeMetodo);
+    console.log('---->', descriptor);
+    // return {
+    //     value: function(...args: string[]) {
+    //         return args[0].toUpperCase();
+    //     },
+    // }
+}
+
+export class UmaPessoa {
+    nome: string;
+    sobrenome: string;
+    idade: number;
+
+    constructor(nome: string, sobrenome: string, idade: number) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.idade = idade;
+    }
+
+    @decorador        
+    metodo(msg: string): string {
+        return `${this.nome} ${this.sobrenome}: ${msg}`
+    }
+
+    get nomeCompleto(): string {
+        return this.nome + ' ' + this.sobrenome;
+    }
+
+    set nomeCompleto(valor: string) {
+        const palavras = valor.split(/\s+/g);
+        const primeiroNome = palavras.shift();
+        if(!primeiroNome) return;
+        this.nome = primeiroNome;
+        this.sobrenome = palavras.join(' ');
+    }
+}
+
+const pessoa = new UmaPessoa('Luiz', 'Otavio', 30)
+//pessoa.metodo = () => 'EIIIIIIIIIIIIIIIIIIIIII'
+const metodo = pessoa.metodo('Ola mundo')
+console.log(metodo)
